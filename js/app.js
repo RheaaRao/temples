9d3be0377cc8047a0aa6cfc5d3d79322ceb9966d
RHEAA RAO <RHEAA.RAO@JOURNALISM.CUNY.EDU>
2016-11-16 20:18:36 -0500

    sound logic now

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+
+     //old logic 
 	// for dropdown
 	//var orgs =["Hindu American Foundation", "Hindu Swayamsevak Sangh", "Vishwa Hindu Parishad"];
 
@@ -18,30 +20,67 @@ $(document).ready(function(){
     $('#secondSelect')
     .dropdown();
 
-    $('#appbutton').on("click", function(){
-		var dropdownValue = $('#select').val();
+     //want the button to run after user input from each field
+      $('#appbutton').on("click", function(){
+		var org = $('#orgSelect').val();
+		var year = $('#yearSelect').val();
+		var totalAmount = 0;
+		var reasons = "";
+
+		//running through the dataset depending on the organization selected by user
+
+		var dataSet = _.filter(temples,function(item){
+				return item.Organization == org
+			});
+
+		//looping through the "new dataset" which basically means it is running through the data above based on what is inputted
+
+        var newSet = _.filter(dataSet,function(item){
+        	return item.Year == year
+        });
+
+        //want to make the reasons into a neat list rather than a long, sloppy line
+        //there are two or three different amounts, based on the projects the organizations undertake, so I'm trying the add the amounts instead of list each one. 
+
+		reasons = "1. " + newSet[0].Reason;
+		totalAmount = totalAmount + newSet[0].Amount;
+
+		for( var i = 1; i < newSet.length;i++){
+ 			totalAmount = amount + newSet[i].Amount;
+ 			num = i + 1;
+ 			//reasons = "," + num + "." newSet[i].Reason;
+		};
+
+
+		$('#result').append(totalAmount);
+		$('#result2').append(reasons);
+
+     });
+
+ 
+
+ //old logic
+
+    //$('#appbutton').on("click", function(){
+		//var dropdownValue = $('#select').val();
 		
 		// result box 1: append the Amount
-		var Money = _.find(temples, function(num){
-			return num.Amount == dropdownValue;
-		});
+		//var Money = _.find(temples, function(num){
+		//	return num.Amount == dropdownValue;
+		//});
 
-		$('#result').append(
-			Money.Amount
-		);
+		//$('#result').append(
+		//	Money.Amount
+		//);
 
 		// result box 2: Here I want to put the reasons they're spending the money
 		
 
 
 
-	})
-
 	
 
-});
-
-	
+	//more old logic
 
    //$('#appbutton').on("click", function(){
 	//	var dropdownValue = $('#select').val();
